510d64ac78858cb042b531d01bd2c03d3350908f
facku95 <91614607+facku95@users.noreply.github.com>
2024-05-16 23:58:38 -0300

    ultimo commit dentro de horario para entregar

--- PBII1C2024/src/main/java/com/mycompany/pbii1c2024/Permuta.java
+++ PBII1C2024/src/main/java/com/mycompany/pbii1c2024/Permuta.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.pbii1c2024;
 
 import com.mycompany.pbii1c2024.propiedades.Propiedad;
